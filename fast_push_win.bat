4849a36c04f25be7ac84e3becab258b15da22a58
王贤龙 <sloan.wang@finsmart.sg>
2026-01-21 18:05:29 +0800

    ~0,4datetime:~4,2datetime:~6,2datetime:~8,2datetime:~10,2datetime:~12,2

--- fast_push_win.bat
+++ fast_push_win.bat
@@ -1,7 +1,9 @@
 @echo off
 chcp 65001
-set /p msg=请输入提交信息: 
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "datetime=%%a"
+set "msg=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%"
 git add .
 git commit -m "%msg%"
 git push
+echo 提交成功！
 pause

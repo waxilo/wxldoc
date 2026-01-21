a459765d822847b6b0adf19b3de9851311799d90
王贤龙 <sloan.wang@finsmart.sg>
2026-01-21 18:08:01 +0800

    $(date +m-H:S)

--- fast_push_win.bat
+++ fast_push_win.bat
@@ -1,9 +1,7 @@
 @echo off
 chcp 65001
-for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "datetime=%%a"
-set "msg=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%"
 git add .
-git commit -m "%msg%"
+git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"
 git push
 echo 提交成功！
 pause

793e4176512dac3d62d75459eec2bfe64435a872
王贤龙 <sloan.wang@finsmart.sg>
2026-01-21 18:13:15 +0800

    2026-01-21 18:13:15

--- fast_push_win.bat
+++ fast_push_win.bat
@@ -2,12 +2,10 @@
 chcp 65001 >nul
 setlocal enabledelayedexpansion
 
-:: 使用 PowerShell 获取标准格式的当前时间（推荐！）
 for /f "delims=" %%i in ('powershell -command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') do set "DATETIME=%%i"
 
 git add .
 git commit -m "!DATETIME!"
 git push
 
-echo 提交成功！
 pause
